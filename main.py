e723036645944f1b86081fc711bd8deb180ad6b2
Paul Hake <pauljhake@hotmail.com>
2023-03-12 17:29:06 -0400

    updated dvc config

--- main.py
+++ main.py
@@ -44,12 +44,9 @@ class RowData(BaseModel):
 #config for DVC and Heroku
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    os.system('rm -rf .dvc/cache')
-    os.system('rm -rf .dvc/tmp/lock')
-    os.system('dvc config core.hardlink_lock true')
-    if os.system("dvc pull -q") != 0:
+    if os.system("dvc pull") != 0:
         exit("dvc pull failed")
-    os.system("rm -rf .dvc .apt/usr/lib/dvc")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 #API functions
 @app.get("/")
