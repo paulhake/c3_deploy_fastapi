037f04623320106c0c9136b7c45fe5c32007235d
Paul Hake <pauljhake@hotmail.com>
2023-03-14 11:00:32 -0400

    online inference module clean up test code

--- online_inference.py
+++ online_inference.py
@@ -1,6 +1,6 @@
 import joblib
-from model import inference
 import numpy as np
+from model import inference
 
 def online_inference(row_dict, cat_features):
     """ Run model inferences and return the predictions.
@@ -18,27 +18,28 @@ def online_inference(row_dict, cat_features):
     """
     # load the model
     model = joblib.load('./model/model.pkl')
-    #load encoder
+    # load encoder
     encoder = joblib.load('./model/encoder.pkl')
 
-    row_transformed = list()
-    X_categorical = list()
-    X_continuous = list()
-    #iterate through input dictionary and group as categorical or continuous features
+    row_transformed = []
+    X_categorical = []
+    X_continuous = []
+    # iterate through input dictionary and group as categorical or continuous
+    # features
     for key, value in row_dict.items():
         mod_key = key.replace('_', '-')
         if mod_key in cat_features:
             X_categorical.append(value)
         else:
             X_continuous.append(value)
-    #encode cat features to one hot
+    # encode cat features to one hot
     y_cat = encoder.transform([X_categorical])
-    #format continuous features as array
+    # format continuous features as array
     y_conts = np.asarray([X_continuous])
-    #concat features to single array for inference
+    # concat features to single array for inference
     row_transformed = np.concatenate([y_conts, y_cat], axis=1)
 
     # get inference from model
     preds = inference(model=model, X=row_transformed)
-    #return text indicators for hi or low income
+    # return text indicators for hi or low income
     return 'hi_income' if preds[0] else 'low_income'
