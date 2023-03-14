49a55e6c11c4fb9c98490c87c29a1ecdc3feda8b
Paul Hake <pauljhake@hotmail.com>
2023-03-14 10:27:17 -0400

    update slicing calcs

--- slice_analysis.py
+++ slice_analysis.py
@@ -1,10 +1,9 @@
 """
-This module outputs the performance of the model on slices of the data for categorical features.
+This module calculates model performance on slices of the data for categorical features.
 """
 import os
 import joblib
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from data import process_data
 from model import compute_model_metrics
 
@@ -22,41 +21,39 @@ cat_features = [
     "sex",
     "native-country",
 ]
+# read model artifacts
+model = joblib.load(os.path.join(artifacts_path, 'model.pkl'))
+encoder = joblib.load(os.path.join(artifacts_path, 'encoder.pkl'))
+lb = joblib.load(os.path.join(artifacts_path, 'lb.pkl'))
+
 
 def test_performance():
-    """ Check performance by categorical features """
-
+    # read data and model artifacts
     data = pd.read_csv(data_path)
-    _, test = train_test_split(data, test_size=0.20)
-
-    rf = joblib.load(os.path.join(artifacts_path, 'model.pkl'))
-    encoder = joblib.load(os.path.join(artifacts_path, 'encoder.pkl'))
-    lb = joblib.load(os.path.join(artifacts_path, 'lb.pkl'))
 
     slice_metrics = []
-
+    # slice data by categorical feature
     for feature in cat_features:
-        for cls in test[feature].unique():
-            df_temp = test[test[feature] == cls]
+        for cls in data[feature].unique():
+            df_test = data[data[feature] == cls]
 
             X_test, y_test, _, _ = process_data(
-                df_temp,
+                df_test,
                 cat_features,
                 label="salary",
                 encoder=encoder,
                 lb=lb,
                 training=False)
 
-            y_pred = rf.predict(X_test)
-
-            precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+            preds = model.predict(X_test)
+            # calc metrics for slice
+            precision, recall, fbeta = compute_model_metrics(y_test, preds)
             row = f"{feature} - {cls} :: Precision: {precision: .2f}. Recall: {recall: .2f}. Fbeta: {fbeta: .2f}"
             slice_metrics.append(row)
 
             with open('slice_output.txt', 'w') as file:
                 for row in slice_metrics:
                     file.write(row + '\n')
-
 
 
 if __name__ == '__main__':
